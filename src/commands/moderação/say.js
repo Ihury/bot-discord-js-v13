8078ee4b1a967e395efdf10493ff281c5dc77715
Ihury Costa <64152343+Ihury@users.noreply.github.com>
2021-08-09 15:32:06 -0300

    Adição do comando de contador e edição no say

--- src/commands/moderação/say.js
+++ src/commands/moderação/say.js
@@ -1,6 +1,6 @@
 const Command = require('../../structures/Command')
 
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js')
 
 module.exports = class extends Command {
     constructor(client) {
@@ -8,12 +8,6 @@ module.exports = class extends Command {
             name: 'falar',
             description: 'Faz com que o bot diga alguma mensagem.',
             options: [
-                {
-                    name: 'canal',
-                    type: 'CHANNEL',
-                    description: 'Canal onde a mensagem será enviada.',
-                    required: true
-                },
                 {
                     name: 'mensagem',
                     type: 'STRING',
@@ -24,20 +18,58 @@ module.exports = class extends Command {
         })
     }
 
-    run = (interaction) => {
-        const canal = interaction.options.getChannel('canal')
-        if (!['GUILD_TEXT', 'GUILD_ANNOUCEMENTS'].includes(canal.type)) return interaction.reply({ content: 'ERRO | Informe um canal de texto ou de anúncios!', ephemeral: true })
+    run = async (interaction) => {
+        if (!interaction.member.permissions.has('MANAGE_MESSAGES')) return interaction.reply({ content: 'Você não tem permissão para usar este comando!', ephemeral: true })
 
-        const texto = interaction.options.getString('mensagem')
+        const channels = interaction.guild.channels.cache
+            .filter(c => c.type === 'GUILD_TEXT' && c.permissionsFor(this.client.user.id).has(['SEND_MESSAGES', 'EMBED_LINKS']) && c.permissionsFor(interaction.user.id).has('SEND_MESSAGES'))
 
-        const embed = new MessageEmbed()
-            .setTitle(`Uma mensagem foi enviada neste canal.`)
-            .setDescription(texto)
-            .setColor('#0000ff')
-            .setTimestamp()
+        if (!channels.size) return interaction.reply({ content: 'Não consigo enviar a mensagem em nenhum dos canais do servidor.', ephemeral: true })
 
-        canal.send({ embeds: [embed] })
-            .then(() => interaction.reply({ content: `Mensagem enviada com sucesso no canal \`${canal.name}\`.`, ephemeral: true }))
-            .catch(() => interaction.reply({ content: `ERRO | Erro ao tentar enviar a mensagem no canal.`, ephemeral: true }))
+        const actionRow = new MessageActionRow()
+            .addComponents([
+                new MessageSelectMenu()
+                    .setCustomId('channelSelect')
+                    .setPlaceholder('Selecione um canal')
+                    .addOptions(
+                        channels
+                            .map(c => {
+                                return {
+                                    label: c.name,
+                                    value: c.id
+                                }
+                            })
+                    )
+            ])
+
+        const reply = await interaction.reply({
+            content: '**Selecione abaixo o canal onde deseja enviar a mensagem**',
+            components: [actionRow],
+            fetchReply: true
+        })
+
+        const filter = (i) => i.user.id === interaction.user.id
+        const collector = reply.createMessageComponentCollector({ filter, max: 1, time: (3 * 60000) })
+
+        collector.on('collect', (i) => {
+            const idCanal = i.values[0]
+            const canal = interaction.guild.channels.cache.get(idCanal)
+
+            const texto = interaction.options.getString('mensagem')
+
+            const embed = new MessageEmbed()
+                .setTitle(`Uma mensagem foi enviada neste canal.`)
+                .setDescription(texto)
+                .setColor('#0000ff')
+                .setTimestamp()
+
+            canal.send({ embeds: [embed] })
+                .then(() => interaction.editReply({ content: `Mensagem enviada com sucesso no canal ${canal.toString()}.`, components: [] }))
+                .catch(() => interaction.editReply({ content: `ERRO | Erro ao tentar enviar a mensagem no canal.`, components: [] }))
+        })
+
+        collector.on('end', (collected, reason) => {
+            if (reason === 'time') interaction.editReply({ content: 'O tempo para informar o canal se esgotou!', components: [] })
+        })
     }
 }
---
+++ src/commands/uteis/contador.js
@@ -0,0 +1,67 @@
+const { MessageActionRow, MessageButton } = require('discord.js')
+const Command = require('../../structures/Command')
+
+const actionRow = new MessageActionRow()
+    .addComponents(
+        [
+            new MessageButton()
+                .setStyle('DANGER')
+                .setLabel('-1')
+                .setCustomId('REMOVER'),
+            new MessageButton()
+                .setStyle('SUCCESS')
+                .setLabel('+1')
+                .setCustomId('ADICIONAR'),
+            new MessageButton()
+                .setStyle('PRIMARY')
+                .setLabel('ZERAR')
+                .setCustomId('ZERAR')
+        ]
+    )
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'contador',
+            description: 'Inicia um contador no canal'
+        })
+    }
+
+    run = async (interaction) => {
+        let contagem = 0
+
+        const reply = await interaction.reply({
+            content: `Contagem: \`${contagem}\``,
+            components: [actionRow],
+            fetchReply: true
+        })
+
+        const filter = (b) => b.user.id === interaction.user.id
+        const collector = reply.createMessageComponentCollector({ filter, time: (15000) })
+
+        collector.on('collect', (i) => {
+            switch (i.customId) {
+                case 'REMOVER':
+                    contagem--
+                    break;
+                case 'ADICIONAR':
+                    contagem++
+                    break;
+                case 'ZERAR':
+                    contagem = 0
+                    break;
+            }
+
+            i.update({
+                content: `Contagem: \`${contagem}\``
+            })
+        })
+
+        collector.on('end', (collected, reason) => {
+            if (reason === 'time') interaction.editReply({
+                content: `Contagem finalizada em: \`${contagem}\``,
+                components: []
+            })
+        })
+    }
+}

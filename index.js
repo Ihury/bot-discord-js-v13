6cce0d8f4c63544996a815f1a36f9b432cc021f1
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-08-21 14:28:04 +0000

    chore: add prettier

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ require('dotenv').config()
 const Client = require('./src/structures/Client')
 
 const client = new Client({
-    intents: [
-        'GUILDS',
-        'GUILD_MESSAGE_REACTIONS',
-        'GUILD_MESSAGES',
-        'GUILD_INVITES',
-        'GUILD_VOICE_STATES',
-        'GUILD_MEMBERS',
-        'GUILD_PRESENCES'
-    ]
+  intents: [
+    'GUILDS',
+    'GUILD_MESSAGE_REACTIONS',
+    'GUILD_MESSAGES',
+    'GUILD_INVITES',
+    'GUILD_VOICE_STATES',
+    'GUILD_MEMBERS',
+    'GUILD_PRESENCES',
+  ],
 })
 
 client.login(process.env.BOT_TOKEN)
